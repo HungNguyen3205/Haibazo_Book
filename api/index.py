d45d867bc4606e4dabbe286d6b72db9472dcd969
HungNguyen <namhung03022005@gmail.com>
2026-04-26 18:09:00 +0700

    fix loi khong deploy duoc v15

--- api/index.py
+++ api/index.py
@@ -4,14 +4,16 @@ from database import get_db_connection
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum 
 
+
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"]
 )
 
 class DataPayload(BaseModel):
